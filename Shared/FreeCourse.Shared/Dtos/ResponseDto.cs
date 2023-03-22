e262b04f45f38a4a351bd7213009cd017d4fc008
Tofig Farajullayev <tofiq.ferecullayev@gmail.com>
2023-03-23 00:21:50 +0400

    shared models

---
+++ Shared/FreeCourse.Shared/Dtos/ResponseDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FreeCourse.Shared.Dtos
+{
+    public class ResponseDto<T>
+    {
+        public T Data { get; set; }
+        [JsonIgnore]
+        public int StatusCode { get; set; }
+        public bool IsSuccessful { get; set; }
+    }
+}
