8e5af6e68cd0071162d3d4b9c60e1a7f97350283
Tofig Farajullayev <tofiq.ferecullayev@gmail.com>
2023-04-08 17:44:25 +0400

    basket identityserver4

---
+++ Services/Basket/FreeCourse.Services.Basket/Controllers/BasketsController.cs
@@ -0,0 +1,44 @@
+﻿using FreeCourse.Services.Basket.Dtos;
+using FreeCourse.Services.Basket.Services;
+using FreeCourse.Shared.ControllerBases;
+using FreeCourse.Shared.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Basket.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketsController : CustomBaseController
+    {
+        private readonly IBasketService   _basketService;
+        private readonly IShareIdentityService _shareIdentityService;
+
+        public BasketsController(IBasketService basketService, IShareIdentityService shareIdentityService)
+        {
+            _basketService = basketService;
+            _shareIdentityService = shareIdentityService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBasket()
+        {
+            return CreateActionResultInstance(await _basketService.GetBasket(_shareIdentityService.GetUserId));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveOrUpdate(BasketDto basketDto)
+        {
+
+            var response = await _basketService.SaveOrUpdate(basketDto);
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteBasket()
+        {
+            return CreateActionResultInstance(await _basketService.Delete(_shareIdentityService.GetUserId));
+        }
+    }
+}
