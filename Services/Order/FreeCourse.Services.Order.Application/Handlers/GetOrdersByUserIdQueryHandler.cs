f6125563b1aee79747fc96f6adb821502e5589f9
Tofig Farajullayev <tofiq.ferecullayev@gmail.com>
2023-04-17 02:34:40 +0400

    Handlers queries

---
+++ Services/Order/FreeCourse.Services.Order.Application/Handlers/GetOrdersByUserIdQueryHandler.cs
@@ -0,0 +1,40 @@
+﻿using FreeCourse.Services.Order.Application.Dtos;
+using FreeCourse.Services.Order.Application.Mapping;
+using FreeCourse.Services.Order.Application.Queries;
+using FreeCourse.Services.Order.Infrastructure;
+using FreeCourse.Shared.Dtos;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Order.Application.Handlers
+{
+    public class GetOrdersByUserIdQueryHandler : IRequestHandler<GetOrdersByUserIdQuery, Response<List<OrderDto>>>
+    {
+        private readonly OrderDbContext _dbContext;
+
+        public GetOrdersByUserIdQueryHandler(OrderDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Response<List<OrderDto>>> Handle(GetOrdersByUserIdQuery request, CancellationToken cancellationToken)
+        {
+            var orders= await _dbContext.Orders.Include(i=>i.OrderItems).Where(w=>w.BuyerId == request.UserId).ToListAsync();
+
+            if (!orders.Any())
+            {
+                return Response<List<OrderDto>>.Success(new List<OrderDto>(), 200);
+            }
+
+            var orderDto =  ObjectMapper.Mapper.Map<List<OrderDto>>(orders);
+
+            return Response<List<OrderDto>>.Success(orderDto, 200);
+        }
+    }
+}
