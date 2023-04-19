bcc9ba1819f108b7d4598d6ce219745f96f40401
Tofig Farajullayev <tofiq.ferecullayev@gmail.com>
2023-04-19 12:48:30 +0400

    mvc

---
+++ Frontends/FreeCourse.Web/Models/SigninInput.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace FreeCourse.Web.Models
+{
+    public class SigninInput
+    {
+        [Required]
+        [Display(Name = "Email adress")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember")]
+        public bool IsRemember { get; set; }
+    }
+}
