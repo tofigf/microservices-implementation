8bbd75c9c6e30145fe068de7dd05575fe371eafd
Tofig Farajullayev <tofiq.ferecullayev@gmail.com>
2023-05-01 19:12:15 +0400

    course picture update

--- Frontends/FreeCourse.Web/Controllers/HomeController.cs
+++ Frontends/FreeCourse.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using FreeCourse.Web.Models;
+using FreeCourse.Web.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -12,15 +13,22 @@ namespace FreeCourse.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ICatalogService _catalogService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ICatalogService catalogService)
         {
             _logger = logger;
+            _catalogService = catalogService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return View(await _catalogService.GetAllCourseAsync());
+        }
+
+        public async Task<IActionResult> Detail(string id)
+        {
+            return View(await _catalogService.GetByCourseId(id));
         }
 
         public IActionResult Privacy()
