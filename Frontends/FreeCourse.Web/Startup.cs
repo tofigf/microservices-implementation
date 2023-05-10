1fee880f4b97374ccef5d530fdc8a6b191a5fe7d
Tofig Farajullayev <tofiq.ferecullayev@gmail.com>
2023-05-11 00:15:56 +0400

    fluent validation

--- Frontends/FreeCourse.Web/Startup.cs
+++ Frontends/FreeCourse.Web/Startup.cs
@@ -1,3 +1,4 @@
+using FluentValidation.AspNetCore;
 using FreeCourse.Shared.Services;
 using FreeCourse.Web.Extensions;
 using FreeCourse.Web.Handler;
@@ -5,6 +6,7 @@ using FreeCourse.Web.Helpers;
 using FreeCourse.Web.Models;
 using FreeCourse.Web.Services;
 using FreeCourse.Web.Services.Interfaces;
+using FreeCourse.Web.Validators;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -50,8 +52,8 @@ namespace FreeCourse.Web
                 opts.SlidingExpiration = true;
                 opts.Cookie.Name = "mvcwebcookie";
             });
-      
-            services.AddControllersWithViews();
+
+            services.AddControllersWithViews().AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<CourseCreateInputValidator>());
 
         }
 
---
+++ Frontends/FreeCourse.Web/Validators/CourseUpdateInputValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using FreeCourse.Web.Models.Catalogs;
+
+namespace FreeCourse.Web.Validators
+{
+    public class CourseUpdateInputValidator : AbstractValidator<CourseUpdateInput>
+    {
+        public CourseUpdateInputValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("name can not be null");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("description can not be null");
+            RuleFor(x => x.Feature.Duration).InclusiveBetween(1, int.MaxValue).WithMessage("duration can not be null");
+
+            RuleFor(x => x.Price).NotEmpty().WithMessage("price area can not be null")
+                .ScalePrecision(2, 6).WithMessage("error price format");
+        }
+    }
+}
