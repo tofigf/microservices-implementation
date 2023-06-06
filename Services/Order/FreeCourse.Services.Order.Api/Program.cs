9e3704f45c435ba4b8940fb2b6915c1315c2e81c
Tofig Farajullayev <tofiq.ferecullayev@gmail.com>
2023-06-06 23:56:51 +0400

    mvc docker compose

--- Services/Order/FreeCourse.Services.Order.Api/Program.cs
+++ Services/Order/FreeCourse.Services.Order.Api/Program.cs
@@ -1,11 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using FreeCourse.Services.Order.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace FreeCourse.Services.Order.Api
 {
@@ -13,7 +11,16 @@ namespace FreeCourse.Services.Order.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+                   var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+                var orderDbContext = serviceProvider.GetRequiredService<OrderDbContext>();
+                orderDbContext.Database.Migrate();
+
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
